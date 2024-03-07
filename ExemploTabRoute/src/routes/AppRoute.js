f354df95fdeb0c1d79e64426d2131937a614e808
Cristiano <jcristiano.c@gmail.com>
2024-03-07 20:36:26 -0300

    Versao inicial das Rotas

---
+++ ExemploTabRoute/src/routes/AppRoute.js
@@ -0,0 +1,21 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import HomePage from "../pages/HomePage";
+import SettingsPage from "../pages/SettingsPage/Settings";
+
+const Tab = createBottomTabNavigator();
+
+const AppRoute = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen 
+                    name="Home" 
+                    component={HomePage} />
+                <Tab.Screen
+                    name="Settings"
+                    component={SettingsPage} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
