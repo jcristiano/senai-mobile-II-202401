019b6ee1ab9e9d0af16248b011bb3e37f55667bf
Cristiano <jcristiano.c@gmail.com>
2024-03-14 20:21:09 -0300

    Adicionado estado para email e senha

--- ExemploAuthFirebase/src/screens/Login/Login.tsx
+++ ExemploAuthFirebase/src/screens/Login/Login.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const Login = () => {
+    
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+
     return(
-        <View>
+        <View style={styles.container}>
             <Text>Tela de login</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 20
+    },
+});
 
 export default Login;
