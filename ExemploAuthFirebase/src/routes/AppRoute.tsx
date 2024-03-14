04ba5d15e4332a64b138e5bc212159cd7f3706ff
Cristiano <jcristiano.c@gmail.com>
2024-03-14 20:09:29 -0300

    Criacao da rota inicial

--- ExemploAuthFirebase/src/routes/AppRoute.tsx
+++ ExemploAuthFirebase/src/routes/AppRoute.tsx
@@ -1,2 +1,20 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/Login";
 
+const Stack = createNativeStackNavigator();
+
+const AppRoute = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen 
+                    name="Login" 
+                    component={Login} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default AppRoute;
