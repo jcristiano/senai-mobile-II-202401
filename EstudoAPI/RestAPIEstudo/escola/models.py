632833b200f3096262d97e96b3d9fca74d71b41c
Cristiano <jcristiano.c@gmail.com>
2024-03-21 20:04:42 -0300

    Desenvolvimento API - versao inicial

---
+++ EstudoAPI/RestAPIEstudo/escola/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Aluno(models.Model):
+
+    nome = models.CharField(max_length=30)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
