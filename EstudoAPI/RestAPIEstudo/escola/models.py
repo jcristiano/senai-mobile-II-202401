ba95dc7b6a8de8a30f1a9a97d64969340d712fe5
Cristiano <jcristiano.c@gmail.com>
2024-03-21 22:46:14 -0300

    Criacao do modelo de Curso

--- EstudoAPI/RestAPIEstudo/escola/models.py
+++ EstudoAPI/RestAPIEstudo/escola/models.py
@@ -6,3 +6,22 @@ class Aluno(models.Model):
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
     data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+
+    codigo_curso = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100)
+    nivel = models.CharField(max_length=1, choices=NIVEL, blank=False, null=False)
+
+    def __str__(self):
+        return self.descricao
