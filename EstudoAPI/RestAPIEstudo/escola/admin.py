3fe0a423477b83925c705c9b7f129cec62e78c15
Cristiano <jcristiano.c@gmail.com>
2024-03-21 21:52:39 -0300

    Configuracao do Aluno Admin

--- EstudoAPI/RestAPIEstudo/escola/admin.py
+++ EstudoAPI/RestAPIEstudo/escola/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from escola.models import Aluno
 
-# Register your models here.
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'rg', 'cpf')
+    list_per_page = 20
+
+admin.site.register(Aluno, Alunos)
+
