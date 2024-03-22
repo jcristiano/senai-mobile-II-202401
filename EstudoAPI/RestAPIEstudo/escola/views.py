b19bcf3c8f628fb6ea6667b45da0c815e7c257d6
Cristiano <jcristiano.c@gmail.com>
2024-03-21 21:55:27 -0300

    Criacao do ViewSet de Aluno

--- EstudoAPI/RestAPIEstudo/escola/views.py
+++ EstudoAPI/RestAPIEstudo/escola/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from escola.models import Aluno
+from escola.serializer import AlunoSerializer
 
-# Create your views here.
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Endpoint para exibir dados de alunos"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
