fa73b648f153a752ababfd819fe0dbd0784f4a60
Cristiano <jcristiano.c@gmail.com>
2024-04-04 20:21:44 -0300

    Instalacao do sensors e versao inicial do hook

---
+++ ExemploAcessoCamera/src/hooks/useShakeDetector.js
@@ -0,0 +1,41 @@
+import { Accelerometer } from "expo-sensors";
+import React, { useEffect, useState } from "react";
+
+const useShakeDetector = (tempoParaRepouso=500) => {
+
+    const [ isShaking, setIsShaking ] = useState(false);
+
+    useEffect( () => {
+        var subscription;
+        const corteForcaG = 2;
+        var estaMovimentando = false;
+
+        const handleShake = ({x, y, z}) => {
+            const aceleracao = Math.sqrt( x ** 2 + y ** 2 + z ** 2);
+            if (aceleracao > corteForcaG) {
+                if(!estaMovimentando){
+                    estaMovimentando=true;
+                    setIsShaking(true);
+                    setTimeout( () => {
+                        setIsShaking(false);
+                        estaMovimentando = false;
+                    }, tempoParaRepouso);
+                }
+            }
+        };
+
+        const subscribe = () => {
+            subscription = Accelerometer.addListener(handleShake);
+        }
+
+        const unsubscribe = () => {
+            subscription && subscription.remove();
+        }
+
+    }, []);
+
+    return {
+        isShaking
+    }
+}
+
