a9798ab79ecd2ba8cf2bee1063c59ea835fc3b03
Cristiano <jcristiano.c@gmail.com>
2024-04-12 23:56:06 -0300

    Versionamento firebaseConfig

---
+++ ExemploAuthFirebase/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
