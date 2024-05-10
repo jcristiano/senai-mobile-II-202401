1bcbc0d3edf11dea2c79a7f93189e35cba46c3da
Cristiano <jcristiano.c@gmail.com>
2024-05-09 21:04:27 -0300

    Desenvolvimento

--- app-react-notes-async/src/screens/HomeScreen.tsx
+++ app-react-notes-async/src/screens/HomeScreen.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 
+const APP_KEY_STORAGE = "APP_KEY_MY_NOTES";
+
 const HomeScreen: React.FC = () => {
+
+    const [ notes, setNotes ] = useState<string[]>([]);
+
+    const loadNotes = () => {
+        const notasSalvas = await AsyncStorage.getItem(APP_KEY_STORAGE);
+    }
+
     return(
         <View style={styles.container}>
             <Text>Home Screen</Text>
