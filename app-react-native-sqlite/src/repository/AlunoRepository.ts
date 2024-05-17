65ffeb14522204dd2a817d04c1503b85008b0449
Cristiano <jcristiano.c@gmail.com>
2024-05-16 23:32:08 -0300

    Desenvolvimento

--- app-react-native-sqlite/src/repository/AlunoRepository.ts
+++ app-react-native-sqlite/src/repository/AlunoRepository.ts
@@ -30,4 +30,14 @@ export default class AlunoRepository {
     public async down(): Promise<void> {
         await executeTransation(`DROP TABLE ${this.tableName}`);
     }
+
+    public create(aluno: Aluno): void {
+        const sql: string = `INSERT INTO ${this.tableName} (nome, cpf, idade) VALUES (?, ?, ?)`;
+        const args = [
+            aluno.nome,
+            aluno.cpf,
+            aluno.idade
+        ];
+        const resultado = executeTransation(sql, args);
+    }
 }
