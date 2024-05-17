12d5584fb5194e371e7e62cb328378dec627b46c
Cristiano <jcristiano.c@gmail.com>
2024-05-16 22:36:02 -0300

    Desenvolvimento

--- app-react-native-sqlite/src/database/SQLiteDatabase.ts
+++ app-react-native-sqlite/src/database/SQLiteDatabase.ts
@@ -2,3 +2,12 @@ import * as SQLite from "expo-sqlite/legacy";
 
 
 export const db = SQLite.openDatabase("database.sqlite", "1.0");
+
+export const executeTransation = (
+    sql: string,
+    values?: (string | number | null)[]
+) => {
+    return new Promise<SQLite.SQLResultSet>((resolve, reject) => {
+        
+    });
+}
